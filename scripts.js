76c4055e40221b5460a4dacce80536de9f69a8eb
Developer7452 <developer7452@gmail.com>
2024-10-24 23:21:52 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
 function toggleMenu() {
-    const menu = document.getElementById("dropdown-menu");
-    menu.classList.toggle("show-menu");
+    var menu = document.getElementById("dropdown-menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
 }
