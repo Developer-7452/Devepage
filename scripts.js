432b19b1c4fe510243fc40ea8122bfdb3bd187d5
Developer7452 <developer7452@gmail.com>
2024-10-25 21:53:43 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,11 +5,8 @@ function toggleMenu() {
     } else {
         menu.style.display = "block";
     }
-},
+}
+
 function toggleTheme() {
     document.body.classList.toggle("dark-mode");
-
-    // Cambia el texto del botón según el tema
-    const themeToggle = document.getElementById("theme-toggle");
-    themeToggle.textContent = document.body.classList.contains("dark-mode") ? "Modo Claro" : "Modo Oscuro";
 }
